71adaa42cc24440828ba278737dac0b8432db6bc
AgusBA011 <agustinblanco01@gmail.com>
2020-11-07 19:10:45 -0600

    [Update] - Se pasa el controller de registerLcoation de OO a function

--- API/controllers/registerLocation-Controller.ts
+++ API/controllers/registerLocation-Controller.ts
@@ -1,62 +1,27 @@
-import * as mongoose from 'mongoose';
 
 import {Coordenadas} from './locationsScheme'
 
-export class registerLocationController {
-
-    private static instance: registerLocationController;
-    private database : any;
-
-    private constructor() {
-
-        this.createConnection();
-
-    }
-
-    //Función para establecer la conexión a la base de datos.
-    private createConnection(){
-
-        try
-        {
-            mongoose.connect('mongodb://192.168.0.11:27017/alertOnMe', //Cambiar ip
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                socketTimeoutMS: 2000
-            });
 
 
-            this.database = mongoose.connection;
+export default function registerLocationController(lat : any, long : any, guid : any){
+    
+    getLocation(lat, long, guid);
 
-
-
-            this.database.on('error', () => {
-                console.log("Can't connect to Mongo Database")
-            });
-
-
-            this.database.once('open', ()=> {
-                console.log("Connected to Mongo Database")
-            });
-        } catch (e)
-        {
-            console.log(e);
-        }
-    }
+}
 
     //Función para extraer con la API de gmaps el cantón con la latitud y longitud dada.
-    public getLocation(lat : any, long : any, guid : any){
+    function getLocation(lat : any, long : any, guid : any){
 
         //Averiguar el canton y llamar a registerLocation()
         var canton : String;
 
         canton = "prueba";
 
-        this.registerLocation(lat, long, guid, canton);
+        registerLocation(lat, long, guid, canton);
     }
 
     //Función que inserta en la collection el documento con la información dada.
-    private registerLocation(lat : any, long : any, guid : any, canton : any){
+    function registerLocation(lat : any, long : any, guid : any, canton : any){
 
 
         try {
@@ -93,17 +58,7 @@ export class registerLocationController {
 
     }
 
-    //Función que crea una instancia de la clase registerLocationController
-    public static getInstance() : registerLocationController
-    {
-        if (!this.instance)
-        {
-            this.instance = new registerLocationController();
-        }
-        return this.instance;
-    }
 
-}
 
 
 
