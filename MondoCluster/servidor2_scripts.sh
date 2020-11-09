1f36745995617a0ca470a3cd5e74def016a3d322
Daniel Martinez Bonilla <mabo.daniel@gmail.com>
2020-11-08 21:38:45 -0600

    Commands Daniel

--- MondoCluster/servidor2_scripts.sh
+++
@@ -1,3 +0,0 @@
-#docker run -d -p 27017:27017 --name bravo mongo mongod --port 27017 --configsvr --replSet alertme_cfg --dbpath /data/configdb
-#docker run -d -p 27022:27022 --name ekko mongo mongod --port 27022 --replSet alertme_moravia --shardsvr
-#docker run -d -p 27021:27021 --name lima mongo mongod --port 27021 --replSet alertme_cartago
